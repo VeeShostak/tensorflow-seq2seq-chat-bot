66ffcbf3fc8c40a223dc9572d5b16898bf1335e1
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-03 23:32:47 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+#  h5py (for loading & saving Keras Models after training)
+REQUIRED_PACKAGES = ['Keras==2.0.4',
+                     'h5py==2.7.0']
+
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='Keras trainer application'
+)
